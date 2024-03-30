9263d661b4d67989cca65665f38df5ec13c48dfa
ceyhunergunn <ceyhunergun61@gmail.com>
2024-03-30 16:48:28 +0300

    more settings tested

--- S3: The TypeScript Compailer/src/app.ts
+++ S3: The TypeScript Compailer/src/app.ts
@@ -37,7 +37,14 @@ console.log(userName);
 
 const button = document.querySelector("button")!;
 
+function add(n1: number, n2: number) {
+  if (n1 + n2 > 0) {
+    return n1 + n2;
+  }
+}
+
 function clickHandler(message: string) {
+  // let userName = "Ceyhun";
   console.log("Clicked! " + message);
 }
 button?.addEventListener("click", clickHandler.bind(null, "Ceyhun"));
