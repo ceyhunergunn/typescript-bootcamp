4f857550cce2d1c91c94a3af86d444c9e91e6cf5
ceyhunergunn <ceyhunergun61@gmail.com>
2024-04-02 18:41:51 +0300

    Section 5: Classes & Interfaces %20 completed.

--- S5: Classes & Interfaces/src/app.ts
+++ S5: Classes & Interfaces/src/app.ts
@@ -1,1 +1,25 @@
-// Code goes here!
+/* ***************** Creating a First Class *************************** */
+
+class Department {
+  name: string;
+
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log("Department: " + this.name);
+  }
+}
+
+const accounting = new Department("Accounting");
+console.log(accounting);
+
+accounting.describe();
+
+const accountingCopy = {
+  name: "DUMMY",
+  describe: accounting.describe,
+};
+
+accountingCopy.describe();
