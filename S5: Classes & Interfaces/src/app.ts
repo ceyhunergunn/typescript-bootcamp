35481a4e349e6038a0a818267cdd8dbb93ee8790
ceyhunergunn <ceyhunergun61@gmail.com>
2024-04-04 02:39:15 +0300

    Section 4: Classes&Interfaces %50 completed

--- S5: Classes & Interfaces/src/app.ts
+++ S5: Classes & Interfaces/src/app.ts
@@ -1,25 +1,76 @@
-/* ***************** Creating a First Class *************************** */
-
 class Department {
-  name: string;
+  //   private id: string;
+  //   private name: string;
+  protected employees: string[] = [];
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(private readonly id: string, public name: string) {
+    this.id = id;
+    this.name = name;
   }
 
   describe(this: Department) {
-    console.log("Department: " + this.name);
+    console.log(`Department (${this.id}): ${this.name}`);
+  }
+
+  addEmployee(employee: string) {
+    //validation etc
+    // this.id = "d2";
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
-const accounting = new Department("Accounting");
-console.log(accounting);
+class ITDepartment extends Department {
+  admins: string[];
+  constructor(id: string, admins: string[]) {
+    super(id, "IT");
+    this.admins = admins;
+  }
+}
 
-accounting.describe();
+class AccountingDepartment extends Department {
+  constructor(id: string, private reports: string[]) {
+    super(id, "Accounting");
+  }
 
-const accountingCopy = {
-  name: "DUMMY",
-  describe: accounting.describe,
-};
+  addEmployee(name: string) {
+    if (name === "Max") {
+      return;
+    }
+    this.employees.push(name);
+  }
+  addReport(text: string) {
+    this.reports.push(text);
+  }
 
-accountingCopy.describe();
+  getReport() {
+    console.log(this.reports);
+  }
+}
+
+const it = new ITDepartment("d1", ["Max"]);
+const accounting = new AccountingDepartment("d2", []);
+
+it.addEmployee("Max");
+it.addEmployee("Manu");
+// it.employees[2] = "Anna";
+console.log(it);
+
+it.describe();
+it.printEmployeeInformation();
+
+accounting.addReport("Something went wrong");
+accounting.getReport();
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
+
+// const accountingCopy = {
+//   name: "DUMMY",
+//   describe: accounting.describe,
+// };
+
+// accountingCopy.describe();
