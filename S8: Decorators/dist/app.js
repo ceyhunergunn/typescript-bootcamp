279e9ad079728996164b3b895c79f4721a84db64
ceyhunergunn <ceyhunergun61@gmail.com>
2024-04-15 16:35:00 +0300

    S8: Decorator completed.

--- S8: Decorators/dist/app.js
+++ S8: Decorators/dist/app.js
@@ -1,4 +1,30 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -8,6 +34,15 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __param = (this && this.__param) || function (paramIndex, decorator) {
     return function (target, key) { decorator(target, key, paramIndex); }
 };
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 function Logger(logString) {
     return function (constructor) {
         console.log(logString);
@@ -15,14 +50,25 @@ function Logger(logString) {
     };
 }
 function WithTemplate(template, hookId) {
-    return function (constructor) {
-        console.log("Rendering Template");
-        var hookEl = document.getElementById(hookId);
-        var p = new constructor();
-        if (hookEl) {
-            hookEl.innerHTML = template;
-            hookEl.querySelector("h1").textContent = p.name;
-        }
+    return function (originalConstructor) {
+        return (function (_super) {
+            __extends(class_1, _super);
+            function class_1() {
+                var _ = [];
+                for (var _i = 0; _i < arguments.length; _i++) {
+                    _[_i] = arguments[_i];
+                }
+                var _this = _super.call(this) || this;
+                console.log("Rendering Template");
+                var hookEl = document.getElementById(hookId);
+                if (hookEl) {
+                    hookEl.innerHTML = template;
+                    hookEl.querySelector("h1").textContent = _this.name;
+                }
+                return _this;
+            }
+            return class_1;
+        }(originalConstructor));
     };
 }
 var Person = (function () {
@@ -91,4 +137,95 @@ var Product = (function () {
     ], Product.prototype, "getPriceWithTax", null);
     return Product;
 }());
+var p1 = new Product("Book", 19);
+var p2 = new Product("Book 2", 23);
+function Autobind(_, _2, descriptor) {
+    var originalMethod = descriptor.value;
+    var adjDescriptor = {
+        configurable: true,
+        enumerable: true,
+        get: function () {
+            var boundFn = originalMethod.bind(this);
+            return boundFn;
+        },
+    };
+    return adjDescriptor;
+}
+var Printer = (function () {
+    function Printer() {
+        this.message = "This works!";
+    }
+    Printer.prototype.showMessage = function () {
+        console.log(this.message);
+    };
+    __decorate([
+        Autobind
+    ], Printer.prototype, "showMessage", null);
+    return Printer;
+}());
+var p = new Printer();
+var button = document.querySelector("button");
+button.addEventListener("click", p.showMessage);
+var registeredValidators = {};
+function Required(target, propName) {
+    var _a;
+    var _b, _c;
+    registeredValidators[target.constructor.name] = __assign(__assign({}, registeredValidators[target.constructor.name]), (_a = {}, _a[propName] = __spreadArray(__spreadArray([], ((_c = (_b = registeredValidators[target.constructor.name]) === null || _b === void 0 ? void 0 : _b[propName]) !== null && _c !== void 0 ? _c : []), true), [
+        "required",
+    ], false), _a));
+}
+function PositiveNumber(target, propName) {
+    var _a;
+    var _b, _c;
+    registeredValidators[target.constructor.name] = __assign(__assign({}, registeredValidators[target.constructor.name]), (_a = {}, _a[propName] = __spreadArray(__spreadArray([], ((_c = (_b = registeredValidators[target.constructor.name]) === null || _b === void 0 ? void 0 : _b[propName]) !== null && _c !== void 0 ? _c : []), true), [
+        "positive",
+    ], false), _a));
+}
+function validate(obj) {
+    var objValidatorConfig = registeredValidators[obj.constructor.name];
+    if (!objValidatorConfig) {
+        return true;
+    }
+    var isValid = true;
+    for (var prop in objValidatorConfig) {
+        for (var _i = 0, _a = objValidatorConfig[prop]; _i < _a.length; _i++) {
+            var validator = _a[_i];
+            switch (validator) {
+                case "required":
+                    isValid = isValid && !!obj[prop];
+                    break;
+                case "positive":
+                    isValid = isValid && obj[prop] > 0;
+                    break;
+            }
+        }
+    }
+    return isValid;
+}
+var Course = (function () {
+    function Course(t, p) {
+        this.title = t;
+        this.price = p;
+    }
+    __decorate([
+        Required
+    ], Course.prototype, "title", void 0);
+    __decorate([
+        PositiveNumber
+    ], Course.prototype, "price", void 0);
+    return Course;
+}());
+var courseForm = document.querySelector("form");
+courseForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    var titleEl = document.getElementById("title");
+    var priceEl = document.getElementById("price");
+    var title = titleEl.value;
+    var price = +priceEl.value;
+    var createdCourse = new Course(title, price);
+    if (!validate(createdCourse)) {
+        alert("Invalid Input!");
+        return;
+    }
+});
 //# sourceMappingURL=app.js.map
