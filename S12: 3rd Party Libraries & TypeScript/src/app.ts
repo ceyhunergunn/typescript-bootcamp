3c33fd490f05afcafbd49b089dc920b29b193f4b
ceyhunergunn <ceyhunergun61@gmail.com>
2024-04-30 13:54:04 +0300

    S12: 3rd Party Libraries & TypeScript completed.

--- S12: 3rd Party Libraries & TypeScript/src/app.ts
+++ S12: 3rd Party Libraries & TypeScript/src/app.ts
@@ -1,1 +1,38 @@
-// Code goes here!
+// import _ from "lodash";
+
+// declare var GLOBAL: any;
+
+// console.log(_.shuffle([1, 2, 3]));
+
+// console.log(GLOBAL);
+
+import "reflect-metadata";
+import { plainToClass } from "class-transformer";
+import { validate } from "class-validator";
+import { Product } from "./product.model";
+
+const products = [
+  { title: "A Carpet", price: 29.99 },
+  { title: "A Book", price: 10.99 },
+];
+
+const newProd = new Product("", -5);
+validate(newProd).then((errors) => {
+  if (errors.length > 0) {
+    console.log("Validation failed");
+    console.log(errors);
+  } else console.log(newProd.getInformation());
+});
+
+// const loadedProducts = products.map((prod) => {
+//   return new Product(prod.title, prod.price);
+// });
+
+const loadedProducts = plainToClass(Product, products);
+
+for (const product of loadedProducts) {
+  console.log(product.getInformation());
+}
+
+// const p1 = new Product("A Book", 12.99);
+// console.log(p1.getInformation());
